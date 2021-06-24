5a7ac1eff474c9f446150d430a15900464d5d98a
radlee <prof.lolito@gmail.com>
2021-06-24 23:08:42 +0200

    Initial Commit

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+export EMAIL_USER="prof.lolito@gmail.com"
+export EMAIL_PASS="Leander247365"
---
+++ mydata.py
@@ -0,0 +1,32 @@
+posts = [
+    {
+        'author': 'Anita Jason',
+        'title': 'We Got Them',
+        'content': 'This is to welcome the Majors',
+        'date_posted': 'April 19 2021'
+    },
+    {
+        'author': 'Pro Verb',
+        'title': 'Writers Club',
+        'content': 'Welcome to the Writers Club',
+        'date_posted': 'April 21 2021'
+    },
+    {
+        'author': 'Major Son',
+        'title': 'Unyana Ka Major',
+        'content': 'Spethu Saida',
+        'date_posted': 'April 23 2021'
+    },
+    {
+        'author': 'Bala Lulama',
+        'title': 'GoCrazy Club',
+        'content': 'Welcome to the our Club',
+        'date_posted': 'April 29 2021'
+    },
+    {
+        'author': 'Shawn Mbulawa',
+        'title': 'Mr Paiger',
+        'content': 'me aand my team breakers',
+        'date_posted': 'April 30 2021'
+    },
+]
